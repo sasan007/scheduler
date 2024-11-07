42f419c7e6d60e005d888735b0386e9562f10ad4
Sasan Karimi <Sasankarimi92@gmail.com>
2024-11-07 11:18:08 +0330

    first

---
+++ src/app/appointment2/time-line/time-line.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-line',
+  standalone: true,
+  imports: [],
+  templateUrl: './time-line.component.html',
+  styleUrl: './time-line.component.css'
+})
+export class TimeLineComponent {
+
+}
